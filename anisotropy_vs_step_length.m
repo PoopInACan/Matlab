7904686a8f5d3956ac5f4469e7d5f6ee4e2f6d2e
PoopInACan <kevme20@dhcp-173.twilight.ifm.liu.se>
2017-02-17 09:48:04 +0100

    Added mobility with error bars

--- anisotropy_vs_step_length.m
+++ anisotropy_vs_step_length.m
@@ -1,3 +1,4 @@
+%% Loading data
 clear;
 clc;
 filename = '../../Data/Defect Positions on Raman/anisotropy_vs_step_length_values.txt';
@@ -58,20 +59,30 @@ prettyPlotLoop(figure(3),30,'yes')
 %% Plot mobility vs step length
 close all force
 fig = figure('Units', 'pixels', ...
-    'Position', [100 100 500 375]);
+    'Position', [500 100 750 605]);
 clf;
 % errorbar(mean_free_path(ind)*1e9,anisotropy(ind)*100,15*ones(1,length(mean_free_path(ind))),'o')
-he = errorbar(1:length(mu_y),mu_x,sigma_x,'o')
+he = errorbar(log(mfp_over_width(ind)),mu_x(ind),sigma_x(ind),'o')
 hold on;
-errorbar(1:length(mu_y),mu_y,sigma_y,'o')
+errorbar(log(mfp_over_width(ind)),mu_y(ind),sigma_y(ind),'o')
 ax = he.Parent;
 legend('\mu_x','\mu_y')
-for i = 1:length(mu_y)
-    text(i-.1,max([mu_y(i),mu_x(i)])+150,num2str(sampleName(i)))
+for i = ind'
+    text(log(i),max([mu_y(i),mu_x(i)])+150,num2str(sampleName(i)))
 end
 % xlabel('terrace width (\mum)');
-ylabel('Anisotropy (% difference)');
+ylabel('Mobility');
 prettyPlotLoop(figure(1),15,'yes')
-% saveFigure(figure(1),'mean_free_path_vs_anisotropy2','png','/Users/kevme20/Box Sync/PhD/Experiment/Outputs/Presentationpng/')
+hold off;
+saveFigure(figure(1),'mobility with error bars','png','/Users/kevme20/Box Sync/PhD/Experiment/Outputs/Presentationpng/')
 % /Users/kevme20/Box Sync/PhD/Experiment/Outputs/Presentationpng/
 % print('/Users/kevme20/Box Sync/PhD/Experiment/Outputs/Presentationpng/mean_free_path_vs_a
+%%
+ani = abs((mu_x(ind)-mu_y(ind))./((mu_x(ind)+mu_y(ind))/2));
+ind = find(ani<.6);
+ani = ani(ind);
+x = abs(mean_free_path(ind)./(mean_free_path(ind)-terrace_width(ind)));
+[a_fit,sig_a,yy,chisqr] = linreg(x',ani',ones(1,length(x)));
+figure(1);clf;
+plot(abs(mean_free_path(ind)./(mean_free_path(ind)-terrace_width(ind))),ani,'o',x,yy)
+ylim([0 .5])
